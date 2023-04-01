320161f7ee089a7a9e7313001fd272154a43cffb
anisha stha <stha.anisha16@gmail.com>
2023-04-01 09:52:00 +0545

    feat: all the components

--- starter_src/components/Navbar/Navbar.jsx
+++ starter_src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React , {useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.gericht} alt="app__logo" />
+        <img src={images.gericht} alt="app logo" />
       </div>
       <ul className="app__navbar-links">
         <li className="p__opensans">
@@ -30,7 +30,7 @@ const Navbar = () => {
       </ul>
       <div className="app__navbar-login">
         <a href="#login" className="p__opensans">
-          Log In / Registration
+          Log In / Register
         </a>
         <div />
         <a href="/" className="p__opensans">
